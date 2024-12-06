c241757ca55e1ea2b7d3687ad9af9c22d9569c26
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-12-05 21:17:28 -0500

    Custom Text y Default Props

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
 import React from 'react'
 import {globalStyles} from "@/styles/global-styles";
+import ThemeText from "@/components/ThemeText";
 
 const CalculatorApp = () => {
 
     return (
         <View style={globalStyles.calculatorContainer}>
-            <Text style={globalStyles.mainResult}>CalculatorApp</Text>
-            <Text style={globalStyles.subResult}>subResult</Text>
+            <ThemeText variant={'h1'}>
+                50 x 50000
+            </ThemeText>
+            <ThemeText variant={'h2'}>
+                2500000
+            </ThemeText>
         </View>
     )
 }
